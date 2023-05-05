ece8f900e5bce3955402535598480f16ba30c4dc
cimoDevOps <m.ammaha2144@uca.ac.ma>
2023-05-05 18:19:07 +0100

    Added my project

---
+++ CloudGateway/src/main/java/com/Sk09Team/CloudGateway/controller/FallbackController.java
@@ -0,0 +1,16 @@
+package com.Sk09Team.CloudGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+    /*
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback() {
+        return "Order Service is down!";
+
+    }
+*/
+}
